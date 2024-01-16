19f18640c0a8f09e5220e78906bc5ce863a0360b
Augustin Boissier <aboissie@stanford.edu>
2024-01-16 11:21:15 -0800

    Provided Solutions in C++

--- Daily LeetCode Problems/23-01-14 - Determine if Two Strings Are Close/Determine if Two Strings are similar/src/Solution.java
+++ Daily LeetCode Problems/23-01-14 - Determine if Two Strings Are Close/Determine if Two Strings are similar/src/Solution.java
@@ -1,12 +1,12 @@
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Set;
 
 class Solution {
     private static HashMap<Character, Integer> Counter(String word){
         HashMap<Character, Integer> freqCounter = new HashMap<>();
         for(Character c: word.toCharArray()){
-            if(freqCounter.containsKey(c)) freqCounter.put(c, freqCounter.get(c) + 1);
-            else freqCounter.put(c, 1);
+            freqCounter.put(c, freqCounter.getOrDefault(c, 0) + 1);
         }
         return freqCounter;
     }
@@ -14,15 +14,18 @@ class Solution {
     public boolean closeStrings(String word1, String word2) {
         HashMap<Character, Integer> freqCounter1 = Counter(word1);
         HashMap<Character, Integer> freqCounter2 = Counter(word2);
-        
-        Integer[] res1 = new Integer[freqCounter1.values().size()];
-        Integer[] res2 = new Integer[freqCounter2.values().size()];
-        Integer[] freq1 =  freqCounter1.values().toArray(res1);
-        Integer[] freq2 =  freqCounter2.values().toArray(res2);
+
+        // Check if both words have the same set of characters
+        if (!freqCounter1.keySet().equals(freqCounter2.keySet())) {
+            return false;
+        }
+
+        Integer[] freq1 = freqCounter1.values().toArray(new Integer[0]);
+        Integer[] freq2 = freqCounter2.values().toArray(new Integer[0]);
 
         Arrays.sort(freq1);
         Arrays.sort(freq2);
 
-        return freq1 == freq2;
+        return Arrays.equals(freq1, freq2);
     }
 }
